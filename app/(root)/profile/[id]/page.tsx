edfc301465566e4fb8fcf829a6f988d29772155d
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-03-05 02:16:41 +0530

    Added the tab section in profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,9 @@
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/_/ui/tabs"
 import ProfileHeader from "@/components/forms/ProfileHeader";
+import { profileTabs } from "@/constants";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 async function Page({params}:{params: {id: string}}) {
@@ -10,20 +12,47 @@ async function Page({params}:{params: {id: string}}) {
     if(!user) return null;
 
         const userInfo = await fetchUser(params.id);
+        // ...
+        if (!userInfo?.onboarded) redirect('/onboarding'); // Use the 'redirect' function
 
-        if(!userInfo?.onboarded) redirect('/onboarding');
-    return(
+        return (
             <section>
                 <ProfileHeader
-                accountId={userInfo.userId}
-                authUserId={user.id}
-                name={userInfo.name} // Add 'name' property to 'User' type
-                userName={userInfo.userName}
-                imgUrl={userInfo.image}
-                bio={userInfo.bio}
+                    accountId={userInfo.userId}
+                    authUserId={user.id}
+                    name={userInfo.name} // Add 'name' property to 'User' type
+                    userName={userInfo.userName}
+                    imgUrl={userInfo.image}
+                    bio={userInfo.bio}
                 />
+                <div className="mt-9">
+                    <Tabs defaultValue="threads" className="w-full">
+                        <TabsList className="tab">
+                            {profileTabs.map((tab) => (
+                                <TabsTrigger
+                                    key={tab.label}
+                                    value={tab.value}
+                                    className="tab"
+                                >
+                                    <Image
+                                        src={tab.icon}
+                                        alt={tab.label}
+                                        height={24}
+                                        width={24}
+                                        className="object-contain"
+                                    />
+                                    <p className="max-sm:hidden">{tab.label}</p>
+
+                                    {tab.label === "Threads" && (
+                                        <p className="ml-1 px-2 py-1 bg-light-4 rounded-sm !text-tiny-medium text-light-2">{userInfo?.threads?.length}</p>
+                                    )}
+                                </TabsTrigger>
+                            ))}
+                        </TabsList>
+                    </Tabs>
+                </div>
             </section>
-        )
+        );
 }
 
 export default Page;
